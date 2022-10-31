68dbd55552a23d4918808d1f248fd3a09168df9c
sho037 <103867061+sho037@users.noreply.github.com>
2022-11-01 02:00:14 +0900

    ベース(first commit)

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+// This program is DB
+int main (void) {
+  char operation;
+  printf("こんにちは\n");
+  printf("どの操作を行いますか？\nDB追加(A), DB削除(B), DB探索(C)\n");
+  scanf("%s", operation);
+  if (operation == "DB追加" || operation == "A")
+  {
+    /* code */
+  }
+  else if (operation == "DB削除" || operation == "B")
+  {
+    /* code */
+  }
+  else if (operation == "DB探索" || operation == "C")
+  {
+    /* code */
+  }
+  
+
+  return 0;
+}
