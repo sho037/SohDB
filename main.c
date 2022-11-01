c9ad12ac6de3c1c7e42530b6f188b9d1c417331c
sho037 <103867061+sho037@users.noreply.github.com>
2022-11-01 12:06:29 +0900

    do while文で繰り返し処理に

--- main.c
+++ main.c
@@ -19,27 +19,41 @@ int main(void)
 {
   char operation[ARRAYSIZE];
   char filename[ARRAYSIZE];
+  char yesorno[32];
   printf("こんにちは\n");
-  printf("どの操作を行いますか？\nDB追加(A), DB更新(B), DB探索(C)\n");
-  printf("%s", operation);
-  if (strcmp(operation, "DB追加") == 0 || strcmp(operation, "A") == 0)
+  
+  do
   {
-    printf("データベースを追加するため、ファイル名を決めてください。\n");
-    scanf("%s", filename);
-    addDataBase(filename);
-  }
-  else if (strcmp(operation, "DB更新") == 0 || strcmp(operation, "B") == 0)
-  {
-    printf("データベースを更新するため、ファイル名を入力してください\n");
-    scanf("%s", filename);
-    updateDataBase(filename);
-  }
-  else if (strcmp(operation, "DB探索") == 0 || strcmp(operation, "C") == 0)
-  {
-    printf("データベースを探索するため、ファイル名を入力してください\n");
-    scanf("%s", filename);
-    serchDataBase(filename);
-  }
+    printf("どの操作を行いますか？\nDB追加(A), DB更新(B), DB探索(C)\n");
+    scanf("%s", operation);
+    printf("%s", operation);  // develop
+    if (strcmp(operation, "DB追加") == 0 || strcmp(operation, "A") == 0)
+    {
+      printf("データベースを追加するため、ファイル名を決めてください。\n");
+      scanf("%s", filename);
+      addDataBase(filename);
+      printf("データベースの追加を終了します。\n");
+    }
+    else if (strcmp(operation, "DB更新") == 0 || strcmp(operation, "B") == 0)
+    {
+      printf("データベースを更新するため、ファイル名を入力してください\n");
+      scanf("%s", filename);
+      updateDataBase(filename);
+      printf("データベースの更新を終了します。\n");
+    }
+    else if (strcmp(operation, "DB探索") == 0 || strcmp(operation, "C") == 0)
+    {
+      printf("データベースを探索するため、ファイル名を入力してください\n");
+      scanf("%s", filename);
+      serchDataBase(filename);
+      printf("データベースの探索を終了します。\n");
+    } else {
+      printf("正しく入力を行ってください\n");
+      continue;
+    }
+    printf("データベースの操作を続けますか？(Y/n)\n");
+    scanf("%s", yesorno);
+  } while (strcmp(yesorno, "Y") == 0 || strcmp(yesorno, "y") == 0);
 
   return 0;
 }
