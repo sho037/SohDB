e9b37839bc7b0a0153b2a98e6d6f944d44acb243
sho037 <103867061+sho037@users.noreply.github.com>
2022-11-01 10:24:45 +0900

    操作を変更

--- main.c
+++ main.c
@@ -23,23 +23,19 @@ int main(void)
   char *operation;
   char *filename;
   printf("こんにちは\n");
-  printf("どの操作を行いますか？\nDB追加(A), DB削除(B), DB探索(C)\n");
+  printf("どの操作を行いますか？\nDB追加(A), DB更新(B), DB探索(C)\n");
   scanf("%s", operation);
   if (strcmp(operation, "DB追加") == 0 || strcmp(operation, "A") == 0)
   {
     addDataBase();
   }
-  else if (strcmp(operation, "DB削除") == 0 || strcmp(operation, "B") == 0)
+  else if (strcmp(operation, "DB更新") == 0 || strcmp(operation, "B") == 0)
   {
     removeDataBase();
   }
   else if (strcmp(operation, "DB探索") == 0 || strcmp(operation, "C") == 0)
   {
     serchDataBase();
-  }
-  else if (strcmp(operation, "DB更新") == 0 || strcmp(operation, "D") == 0)
-  {
-    updateDataBase();
   }
 
   return 0;
