9c804b01a84fe8c119d864541ff9389bbae041a9
sho037 <103867061+sho037@users.noreply.github.com>
2022-11-01 12:08:53 +0900

    yかnが入力されるまで繰り返し

--- main.c
+++ main.c
@@ -51,8 +51,11 @@ int main(void)
       printf("正しく入力を行ってください\n");
       continue;
     }
-    printf("データベースの操作を続けますか？(Y/n)\n");
-    scanf("%s", yesorno);
+    do {
+      printf("データベースの操作を続けますか？(Y/n)\n");
+      scanf("%s", yesorno);
+    } while (strcmp(yesorno, "Y") == 0 || strcmp(yesorno, "y") == 0 || strcmp(yesorno, "N") == 0 || strcmp(yesorno, "n") == 0);
+
   } while (strcmp(yesorno, "Y") == 0 || strcmp(yesorno, "y") == 0);
 
   return 0;
